807c013f6957a6bcaa5adc664bc0ade0c27d0b93
Heiko W. Rupp <hwr@pilhuhn.de>
2017-08-14 11:25:31 +0200

    Use http sender

--- src/main/java/io/openshift/booster/TracerSetup.java
+++ src/main/java/io/openshift/booster/TracerSetup.java
@@ -1,6 +1,7 @@
 package io.openshift.booster;
 
 import com.uber.jaeger.samplers.ProbabilisticSampler;
+import com.uber.jaeger.senders.HttpSender;
 import io.opentracing.util.GlobalTracer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,9 +12,16 @@ public class TracerSetup {
 
 	@Bean
 	public Tracer jaegerTracer() {
+		
+		String jaegerHttpUrl = System.getenv("JAEGER_HTTP_QUERY_URL");
+		if (jaegerHttpUrl==null || jaegerHttpUrl.isEmpty()) {
+			jaegerHttpUrl="http://localhost:14268/api/traces";
+		}
+
+		HttpSender httpTracer = new HttpSender(jaegerHttpUrl,0);
 		return new com.uber.jaeger.Configuration("spring-boot",
 				new com.uber.jaeger.Configuration.SamplerConfiguration(
 						ProbabilisticSampler.TYPE, 1),
-				new com.uber.jaeger.Configuration.ReporterConfiguration()).getTracer();
+				new com.uber.jaeger.Configuration.ReporterConfiguration(httpTracer)).getTracer();
 	}
 }
